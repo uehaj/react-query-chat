fe5e99aaa81e63bb2f93a94ab5a6744d9bb1a6cf
UEHARA Junji <uehaj@users.noreply.github.com>
2021-12-09 23:34:55 +0900

    refinment data structure

--- src/components/ChatPanel.tsx
+++ src/components/ChatPanel.tsx
@@ -1,15 +1,16 @@
 import React, { useState } from 'react';
 
 export default function ChatPanel() {
-  const [currentRoomId, setCurrentRoomId] = useState<string>('0000');
-  const rooms: { name: string, id: string }[] = [
-    { name: 'room1', id: '0001' },
-    { name: 'room2', id: '0002' }
+  const [currentRoomId, setCurrentRoomId] = useState<string>('0001');
+  const rooms: { roomId: string, name: string }[] = [
+    { roomId: '0001', name: 'room1' },
+    { roomId: '0002', name: 'room2' }
   ];
-  const messages: { roomId: string, userId: string, content: string }[] = [
-    { roomId: '0001', userId: '1', content: 'こんばんは' },
-    { roomId: '0001', userId: '2', content: 'こんにちは' },
-    { roomId: '0003', userId: '4', content: 'なるほどっ' }
+  const messages: { messageId: string, datetime: string, roomId: string, userId: string, content: string }[] = [
+    { messageId: '0001', datetime: "2021/12/24: 10:00:00", roomId: '0001', userId: '1', content: 'こんばんは' },
+    { messageId: '0002', datetime: "2021/12/24: 11:00:00", roomId: '0001', userId: '2', content: 'こんにちは' },
+    { messageId: '0003', datetime: "2021/12/24: 12:00:00", roomId: '0002', userId: '3', content: 'オレはおります' },
+    { messageId: '0003', datetime: "2021/12/24: 13:00:00", roomId: '0002', userId: '4', content: 'なるほどっ' }
   ];
   const users: { userId: string, name: string }[] = [
     { userId: '1', name: 'たんじろう' },
@@ -23,15 +24,15 @@ export default function ChatPanel() {
       Rooms
       <ul>
         {rooms?.map((room) => (
-          <li key={room.id} onClick={() => setCurrentRoomId(room.id)}>
+          <li key={room.roomId} onClick={() => setCurrentRoomId(room.roomId)}>
             {room.name}
           </li>
         ))}
       </ul>
       Messages
       <ul>
-        {messages?.map((message) => (
-          <li>{message.roomId} : {users.find(user => user.userId === message.userId)?.name} {message.content}</li>
+        {messages?.filter(message => message.roomId === currentRoomId)?.map((message) => (
+          <li>{message.datetime} : {users.find(user => user.userId === message.userId)?.name} {message.content}</li>
         ))}
       </ul>
     </>
